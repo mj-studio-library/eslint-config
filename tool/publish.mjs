c0d0b702b626a0ea365d48fb157f2e6072aaabae
mym0404 <mym0404@gmail.com>
2024-02-19 16:00:44 +0900

    0.0.2

---
+++ tool/publish.mjs
@@ -0,0 +1,21 @@
+#!/usr/bin/env zx
+const v = process.argv[3];
+
+if (!v || !/^\d+\.\d+\.\d+$/.test(v)) {
+  console.log('version required');
+  await $`exit 1`;
+}
+
+const paths = ['packages/node/package.json', 'packages/react/package.json', 'packages/react-native/package.json']
+
+for(const p of paths) {
+  const f = await fs.readFile(p, 'utf-8');
+  const c = JSON.parse(f);
+  c.version = v;
+  await fs.writeFile(p, JSON.stringify(c, null, 2));
+}
+
+await $`git add .`;
+await $`git commit -m ${v}`;
+await $`git tag ${v}`;
+await $`git push && git push ${v}`;
