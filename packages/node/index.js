72f609a55ab48345b329140deaf719ed76fbe40d
mym0404 <mym0404@gmail.com>
2024-02-19 17:09:32 +0900

    0.0.15

--- packages/node/index.js
+++ packages/node/index.js
@@ -4,16 +4,59 @@ module.exports = {
     browser: true,
     jest: true,
   },
-  plugins: ['simple-import-sort', 'import', '@typescript-eslint/eslint-plugin'],
-  parser: "@typescript-eslint/parser",
   parserOptions: {
-    "ecmaFeatures": {
-      "jsx": true
-    },
-    "ecmaVersion": 12,
-    "sourceType": "module"
+    sourceType: 'module',
+    ecmaVersion: 2017,
   },
+  plugins: ['simple-import-sort', 'import'],
   overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      parser: '@typescript-eslint/parser',
+      plugins: ['@typescript-eslint/eslint-plugin'],
+      rules: {
+        'prettier/prettier': ['error', {endOfLine: 'auto'}],
+        'simple-import-sort/imports': [
+          'error',
+          {
+            groups: [
+              ['^\\u0000'],
+              ['^react', '^@?\\w'],
+              ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+              ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+            ],
+          },
+        ],
+        '@typescript-eslint/consistent-type-imports': 'warn',
+        'no-unused-vars': 'off',
+        '@typescript-eslint/no-unused-vars': [
+          'warn',
+          {
+            varsIgnorePattern: '^_',
+            argsIgnorePattern: '^_',
+            ignoreRestSiblings: true,
+          },
+        ],
+        'no-shadow': 'off',
+        '@typescript-eslint/no-shadow': ['warn'],
+        '@typescript-eslint/explicit-function-return-type': 0,
+        'no-duplicate-imports': 'off',
+        'import/no-duplicates': ['error', {'prefer-inline': false}],
+        '@typescript-eslint/no-var-requires': 0,
+        '@typescript-eslint/interface-name-prefix': 0,
+        '@typescript-eslint/no-empty-function': 0,
+        '@typescript-eslint/no-unused-expressions': [
+          'error',
+          {
+            allowShortCircuit: true,
+            allowTernary: true,
+            allowTaggedTemplates: true,
+          },
+        ],
+        '@typescript-eslint/no-use-before-define': 0,
+        '@typescript-eslint/ban-ts-comment': 0,
+      },
+    },
     {
       files: ['*.json'],
       rules: {
@@ -22,26 +65,9 @@ module.exports = {
     },
   ],
   rules: {
-    'simple-import-sort/imports': [
-      'error',
-      {
-        groups: [
-          ['^\\u0000'],
-          ['^react', '^@?\\w'],
-          ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
-          ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
-        ],
-      },
-    ],
-    '@typescript-eslint/consistent-type-imports': 'warn',
-    'no-shadow': 'off',
-    '@typescript-eslint/no-shadow': ['warn'],
-    '@typescript-eslint/explicit-function-return-type': 0,
-    'import/no-duplicates': ['error', {'prefer-inline': false}],
-    '@typescript-eslint/no-var-requires': 0,
     camelcase: 0,
     indent: 'off',
-    '@typescript-eslint/interface-name-prefix': 0,
+
     'no-console': 'warn',
     curly: ['error', 'all'],
     'max-len': [
@@ -60,7 +86,6 @@ module.exports = {
     'no-new-object': 'error',
     'no-array-constructor': 'error',
     'no-use-before-define': 0,
-    '@typescript-eslint/no-use-before-define': 0,
     'space-before-function-paren': [
       'error',
       {
@@ -70,7 +95,6 @@ module.exports = {
       },
     ],
     'generator-star-spacing': ['error', {before: false, after: true}],
-    '@typescript-eslint/ban-ts-comment': 0,
     'padding-line-between-statements': [
       'warn',
       {blankLine: 'always', prev: '*', next: 'return'},
@@ -111,16 +135,8 @@ module.exports = {
         next: '*',
       },
     ],
-    '@typescript-eslint/no-empty-function': 0,
     'no-unused-expressions': 'off',
-    '@typescript-eslint/no-unused-expressions': [
-      'error',
-      {
-        allowShortCircuit: true,
-        allowTernary: true,
-        allowTaggedTemplates: true,
-      },
-    ],
+
     'default-param-last': ['warn'],
     'no-undef': 0,
     'no-duplicate-imports': 'error',
