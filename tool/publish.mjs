d53911004addc14cda6ab289a2d72e8f5063cea7
mym0404 <mym0404@gmail.com>
2024-02-19 16:03:16 +0900

    0.0.3

--- tool/publish.mjs
+++ tool/publish.mjs
@@ -6,9 +6,9 @@ if (!v || !/^\d+\.\d+\.\d+$/.test(v)) {
   await $`exit 1`;
 }
 
-const paths = ['packages/node/package.json', 'packages/react/package.json', 'packages/react-native/package.json']
+const paths = ['packages/node/package.json', 'packages/react/package.json', 'packages/react-native/package.json'];
 
-for(const p of paths) {
+for (const p of paths) {
   const f = await fs.readFile(p, 'utf-8');
   const c = JSON.parse(f);
   c.version = v;
@@ -19,3 +19,9 @@ await $`git add .`;
 await $`git commit -m ${v}`;
 await $`git tag ${v}`;
 await $`git push && git push origin --tags`;
+
+const cur = $`pwd`;
+const ps = [path.join(cur, 'packages/node'), path.join(cur, 'packages/react'), path.join(cur, 'packages/react-native')];
+for (const p of ps) {
+  await $`cd ${p} && yarn publish`;
+}
